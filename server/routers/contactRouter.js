cea630aaea0369f8be391795cac57485eb4465b9
LauraStorm <lbs2rm@hotmail.com>
2023-04-26 21:58:05 +0200

    removed contactRouter.js

--- server/routers/contactRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-router.get("/contact", (req, res) => {
-    res.send({message: `Welcome ${req.session.username} to Contact Page - Write a message`})
-});
-
-export default router;
